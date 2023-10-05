ba91724100907b63512b2ad48af14528d9d35890
VehvilainenPooki <pooki.vehvilainen@helsinki.fi>
2023-10-05 17:30:40 +0300

    Updated gitignore started testing sql

--- source/app.py
+++ source/app.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from flask import render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+db = SQLAlchemy(app)
 
 @app.route("/")
 def home():
