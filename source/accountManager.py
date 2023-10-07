d7ce7a7e139a4b5161ebb16b90e1f9612d4c9df7
VehvilainenPooki <pooki.vehvilainen@helsinki.fi>
2023-10-07 19:02:05 +0300

    Added account exists logic to accountmanager

--- source/accountManager.py
+++ source/accountManager.py
@@ -1,7 +1,26 @@
+from sqlalchemy.sql import text
+
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
+
 from db import db
 
-def check_login():
-    1#todo
+
+
+def check_does_account_exist(username):
+    sql = "SELECT id, password FROM users WHERE username=:username"
+    result = db.session.execute(text(sql), {"username":username})
+    user = result.fetchone()
+    print("Does account exist:")
+    if not user:
+        print("False")
+        return False
+    print("True")
+    return True
+
+def check_is_password_correct():
+    1
 
 def create_account():
     1#todo
--- source/db.py
+++ source/db.py
@@ -6,4 +6,6 @@ from os import getenv
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.secret_key = getenv("SECRET_KEY")
+
 db = SQLAlchemy(app)
