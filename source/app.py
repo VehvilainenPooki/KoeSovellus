5c8d8640a96662b536001de2ec21d1d951e2b1a0
VehvilainenPooki <pooki.vehvilainen@helsinki.fi>
2023-10-07 20:35:17 +0300

    Made little changes to fix no secret_key error.

--- source/app.py
+++ source/app.py
@@ -5,4 +5,6 @@ from os import getenv
 
 app = Flask(__name__)
 
+app.secret_key = getenv("SECRET_KEY")
+
 import routes
--- source/db.py
+++ source/db.py
@@ -6,6 +6,5 @@ from os import getenv
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
-app.secret_key = getenv("SECRET_KEY")
 
 db = SQLAlchemy(app)
