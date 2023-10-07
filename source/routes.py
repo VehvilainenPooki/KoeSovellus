474c602ae2f417ac8441ce90184c3e832bc9e28d
VehvilainenPooki <pooki.vehvilainen@helsinki.fi>
2023-10-07 18:33:44 +0300

    Added templates for login and account creation.

--- source/routes.py
+++ source/routes.py
@@ -1,15 +1,25 @@
 from app import app
 import accountManager
-from flask import render_template
+from flask import redirect, render_template, request, session
 
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home-page.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET","POST"])
 def login():
+    if request.method == "POST":
+        1#todo Login attempt
+
     return render_template("login.html")
+
+@app.route("/account-creation", methods=["GET","POST"])
+def accountCreation():
+    if request.method == "POST":
+        1#todo account creation
+
+    return render_template("account-creation.html")
 
 @app.route("/account-creation")
 def account_creation():
