9001ca9618eb0625d6ee7c984737e467f9bd174d
VehvilainenPooki <pooki.vehvilainen@helsinki.fi>
2023-10-08 21:55:14 +0300

    Changed default admin password to be in hash form.

--- schema.sql
+++ schema.sql
@@ -8,6 +8,6 @@ CREATE TABLE users (
 
 INSERT INTO users(username, password, is_admin) VALUES (
     'admin',
-    'admin',
+    'pbkdf2:sha256:600000$zlItG5J1haRfNwHd$66f470dc9e2de59db62b96cac55661f941875ae8b715d03bcef64a44f0d2927f',
     'true'
 );
