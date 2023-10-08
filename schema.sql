bde9ed59d41ef1b5e8d77619402fbc1e3f79daff
VehvilainenPooki <pooki.vehvilainen@helsinki.fi>
2023-10-08 21:18:28 +0300

    Altered schema so that a default admin user is created

--- schema.sql
+++ schema.sql
@@ -1,5 +1,13 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY, 
     username TEXT UNIQUE, 
-    password TEXT
+    password TEXT,
+    is_admin BOOLEAN
+);
+
+
+INSERT INTO users(username, password, is_admin) VALUES (
+    'admin',
+    'admin',
+    'true'
 );
