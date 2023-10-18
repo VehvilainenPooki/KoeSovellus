56e5f045fa7306ed235d674f3a0ee005f1ae1b51
VehvilainenPooki <pooki.vehvilainen@helsinki.fi>
2023-10-18 20:03:28 +0300

    Changed column types to work with postgresql and added a temporary test exam.

--- schema.sql
+++ schema.sql
@@ -10,17 +10,17 @@ CREATE TABLE exams (
     examname TEXT UNIQUE, 
     start_key TEXT,
     active BOOLEAN,
-    exercises SET,
-    exercise_type set,
-    points SET
+    exercises TEXT[],
+    exercise_type TEXT[],
+    points INTEGER[]
 );
 
 CREATE TABLE exam_results (
     id SERIAL PRIMARY KEY, 
     examname TEXT UNIQUE, 
     username TEXT,
-    score SET,
-    notes SET
+    score INTEGER[],
+    notes TEXT[]
 );
 
 INSERT INTO users(username, password, is_admin) VALUES (
@@ -28,3 +28,13 @@ INSERT INTO users(username, password, is_admin) VALUES (
     'pbkdf2:sha256:600000$zlItG5J1haRfNwHd$66f470dc9e2de59db62b96cac55661f941875ae8b715d03bcef64a44f0d2927f',
     'true'
 );
+
+INSERT INTO exams(examname, start_key, active, exercises, points) VALUES (
+    'Testikoe',
+    'teStiAvain',
+    'true',
+    ['Kerro elämästä', 'Mitä on kuolema', 'Kuka ja mitä', 'Missä on alku', 'Kuka sinä olet'],
+    [10,5,15,23,2]
+);
+
+
